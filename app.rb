769569bb0bd9c5243a0e2343db506043d8e1a84c
Anthony Tilelli <anthony@tilelli.me>
2020-01-31 15:40:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     when "add"
-      "#{ num1 + num2}"
+      "#{ num1 + num2 }"
     when "subtract"
       "#{params[:number1] - params[:number2]}"
     when "multiply"
